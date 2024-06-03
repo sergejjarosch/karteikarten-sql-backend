78b5891a66b2e96da95bc0b7e445d1a63b1752eb
sergejjarosch <sergejjarosch@gmail.com>
2024-06-03 16:01:09 +0200

    Add connection to PostgreSQL, for first time just local

--- src/ConnectDB.java
+++ src/ConnectDB.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ConnectDB {
+import java.sql.*;
+
+
+public class ConnectDB {
+    public static void main(String[] args) {
+        int id = 3;
+        String sql = "SELECT frage, antwort FROM karte WHERE id = ?";
+        String url = "jdbc:postgresql://localhost:5433/karteikarten-local";
+        String username = "postgres";
+        String password = "DataBase";
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, id);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) { // Überprüfen, ob es ein Ergebnis gibt
+                String frage = rs.getString("frage");
+                String antwort = rs.getString("antwort");
+                System.out.println("Frage: " + frage + ", \nAntwort:" + antwort);
+            } else {
+                System.out.println("Kein Eintrag gefunden.");
+            }
+
+            rs.close();
+            pst.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);;
+        }
+    }
 }
---
+++ src/QueryDB.java
@@ -0,0 +1,2 @@
+public class QueryDB {
+}
